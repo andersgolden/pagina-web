563b3029bb569b7def77dcf244e3619945768e9d
anderson <133811754+andersgolden@users.noreply.github.com.>
2024-12-17 15:59:31 -0500

    corregi algunos estilos

--- contact.js
+++ contact.js
@@ -1,87 +1,79 @@
-document.addEventListener("DOMContentLoaded", function(eventobj) {
-    const form = document.getElementById("contactform");
-    const submitBtn = document.getElementById("submitBtn");
+const form = document.getElementById("contactform");
+const submitBtn = document.getElementById("submitBtn");
 
-    if (submitBtn) {
-        submitBtn.addEventListener("click", function(eventobj) {
+submitBtn.addEventListener("click", function(eventobj) {
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+    };
 
-
-            const name = document.getElementById("name").value;
-            const email = document.getElementById("email").value;
-            const phone = document.getElementById("phone").value;
-            const message = document.getElementById("message").value;
-
-            document.getElementById("errorname").style.display = "none";
-            document.getElementById("erroremail").style.display = "none";
-            document.getElementById("errorphone").style.display = "none";
-
-            let isvalid = true;
-
-            if (!name) {
-                document.getElementById("errorname").style.display = "block";
-                isvalid = false;
-            } else {
-                const namePattern = /^[a-zA-Z\s]+$/;
-                if (!namePattern.test(name)) {
-                    document.getElementById("errorname").style.display = "block";
-                    isvalid = false;
-                }
-            }
-
-            if (!phone) {
-                document.getElementById("errorphone").style.display = "block";
-                isvalid = false;
-            } else {
-                const phonePattern = /^\d{10}$/;
-                if (!phonePattern.test(phone)) {
-                    document.getElementById("errorphone").style.display = "block";
-                    isvalid = false;
-                }
-            }
-
-            if (!email) {
-                document.getElementById("erroremail").style.display = "block";
-                isvalid = false;
-            } else {
-                const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                if (!emailPattern.test(email)) {
-                    document.getElementById("erroremail").style.display = "block";
-                    isvalid = false;
-                }
-            }
-
-            if (isvalid) {
-                const formData = {
-                    name: name,
-                    email: email,
-                    phone: phone,
-                    message: message
-                };
-
-                fetch('http://localhost:3000/', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(formData)
-                })
-
-                if (isvalid) {
-                    console.log("Formulario enviado:", { name, email, phone, message });
-                    $('#myModal').modal('show');
-                    form.reset();
-                }
-
-                if (okBtn) {
-                    okBtn.addEventListener("click", function() {
-                        $('#myModal').modal('hide');
-
-                    })
-                };
-
-            }
-
-        });
-
+    if (!validateForm(formData)) {
+        return;
     }
+
+    sendForm(formData);
 });
+
+
+function validateForm(formData) {
+    var isvalid = true;
+
+    var pattern = /^[a-zA-Z\s]+$/;
+    var nameIsValid = validateField(formData.name, pattern, "errorname")
+    if (nameIsValid == false) {
+        isvalid = false;
+    }
+
+    pattern = /^\d{10}$/;
+    var phoneIsvalid = validateField(formData.phone, pattern, "errorphone")
+    if (phoneIsvalid == false) {
+        isvalid = false;
+    }
+
+    pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    var emailIsvalid = validateField(formData.email, pattern, "erroremail")
+    if (emailIsvalid == false) {
+        isvalid = false;
+    }
+
+    return isvalid;
+}
+
+function validateField(value, pattern, errorLabel) {
+    if (!pattern.test(value)) {
+        document.getElementById(errorLabel).style.display = "block";
+        return false;
+    }
+
+    return true;
+}
+
+function sendForm(formData) {
+    fetch('http://localhost:3000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+
+
+    $('#myModal').modal('show');
+    reset();
+
+    if (okBtn) {
+        okBtn.addEventListener("click", function() {
+            $('#myModal').modal('hide');
+
+        })
+    };
+}
+
+function reset() {
+    form.reset();
+    document.getElementById("errorname").style.display = "none";
+    document.getElementById("erroremail").style.display = "none";
+    document.getElementById("errorphone").style.display = "none";
+}
